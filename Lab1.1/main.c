fb3b84aa904ceece79c635cd1f0b571a4501f10b
RatatornAmornwatee <ratatorn.am@gmail.com>
2025-11-27 20:57:36 +0700

    Add Lab1.1

---
+++ Lab1.1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int n , i ;
+
+  if( scanf( "%d" , &n ) != 1 ||  n <= 0  ) {
+    return 0 ;
+  }
+
+  for( i = 0 ; i < n ; i++ ) {
+    printf( "[%d] Hello world" , n + 1 ) ;
+  }
+
+  return 0 ;
+}
