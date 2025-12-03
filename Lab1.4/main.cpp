78da3e72c2b54aa9826158279a819ff537c76cdf
RatatornAmornwatee <ratatorn.am@gmail.com>
2025-12-03 15:31:26 +0700

    Add Lab1.4

---
+++ Lab1.4/main.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int GetSet( int [] ) ;
+
+int main() {
+    int *data ; 
+    int num ;
+    num = GetSet( data ) ;
+
+    return 0 ;
+}// end main function
+
+int GetSet( int data[] ) {
+    int num ;
+
+    if ( scanf( "%d" , &num ) != 1 ) {
+        printf( "Error" ) ;
+        return 0 ;
+    }// end if
+
+    if ( num <= 0 ) {
+        printf( "Error" ) ;
+        return 0 ;
+    }// end if
+
+    int i =  0 ;
+
+    for( i = 0 ; i < num ; i++ ) {
+        scanf( "%d" , &data[i] ) ;
+    }//end for loop
+
+    return num ;
+}// end GetSet function
