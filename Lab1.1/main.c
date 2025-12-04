3e5d7507d3568a75f8607a80e91c241dc0890a0e
RatatornAmornwatee <ratatorn.am@gmail.com>
2025-12-04 17:26:08 +0700

    Fix Lab1.1

--- Lab1.1/main.c
+++ Lab1.1/main.c
@@ -3,14 +3,19 @@
 int main() {
     int n , i ;
 
-    if ( scanf( "%d" , &n ) != 1 ||  n <= 0  ) {
+    if ( scanf( "%d" , &n ) != 1 ) {
         printf( "Error" ) ;
         return 0 ;
-    }//end if
+    }// end if
+
+    if ( n <= 0 ) {
+        printf( "Error" ) ;
+        return 0 ;
+    }// end if
 
     for ( i = 0 ; i < n ; i++ ) {
         printf( "[%d] Hello world\n" , i + 1 ) ;
-    }//end for
+    }// end for
 
     return 0 ;
-}//end main function 
+}// end main function 
