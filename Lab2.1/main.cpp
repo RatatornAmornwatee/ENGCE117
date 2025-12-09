6203ab2ade06b148df4233e7a5f54a21b1dd0533
RatatornAmornwatee <ratatorn.am@gmail.com>
2025-12-09 22:49:46 +0700

    Fix Lab2.1

--- Lab2.1/main.cpp
+++ Lab2.1/main.cpp
@@ -4,19 +4,25 @@
 void reverse( char str1[] , char str2[] ) ;
 
 int main() {
+
     char text[ 50 ] = "I Love You" ;
     char out[ 50 ] ;
     reverse( text , out ) ;
+
+    printf( "%s" , out ) ;
+
     return 0 ;
-}
+}// end main function
 
 void reverse( char str1[] , char str2[] ) {
+
     int str_size = strlen( str1 ) ;
     int i = 0 ;
 
     for( i = 0 ; i < str_size ; i++ ) {
         str2[ str_size - i - 1 ] = str1[ i ] ;
-    }
+    }// end for loop
 
     str2[ str_size + 1 ] = '\0' ;
-}
+
+}// end reverse function
